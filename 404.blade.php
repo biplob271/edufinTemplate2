08d7a512a33d845b3fd064b6399c5ea55d3416de
Shahinur Rahman Biplob <69090083+biplob271@users.noreply.github.com>
2024-01-24 22:10:49 +0600

    404 Modified

--- 404.blade.php
+++ 404.blade.php
@@ -1,13 +1,31 @@
 
                  
-@include('frontend.layouts.header')
-@include('frontend.layouts.sections.nav')
+@extends('frontend.layouts.main')
+
+@section('page-content')
 </br>
               
 
  <p style="text-align:center; margin-top : 20px">{{$msg}}</p>
 
+
+
 </br>
 
-                               
- @include('frontend.layouts.footer')
+<div class="h-100 d-flex align-items-center justify-content-center">
+  <div style="background:red">
+  <a
+    name=""
+    id=""
+    class="btn btn-success"
+    href="{{route('EditLicense')}}"
+    role="button"
+    >Add New License</a>
+  </div>
+</div>
+
+
+  
+
+</br>
+@endsection
